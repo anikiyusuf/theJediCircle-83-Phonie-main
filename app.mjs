ddd5a8d4141eca8344781392d2a1aa0be119fda3
Aniki Yusuf <anikiyusuf130@gmail.com>
2022-07-25 12:44:29 +0100

    first commit

--- app.mjs
+++ app.mjs
@@ -2,16 +2,51 @@ function startApp() {
     // Your entire app should not necessarily be coded inside this 
     // single function (though there's no penalty for that), 
     // so create and use/call additional functions from here
-  
+
     // pls remove the below and make some magic in here!
     console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
-    }
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
+
+    const carrierCode = document.getElementById('carrier_code');
+    const logo = document.getElementById('logo');
+
+
+
+    carrierCode.addEventListener('change', function(event) {
+        if (event.target.value.length == 11) {
+            let theValue = event.target.value.toString()
+            let newValue = theValue.slice(0, 4)
+            if (newValue == "0803" ||
+                newValue == "0806" ||
+                newValue == "0813" ||
+                newValue == "0706"
+            ) {
+                logo.setAttribute("src", "./MTNLOGO.png")
+                return
+
+            }
+            if (newValue == "0802" ||
+                newValue == "0812" ||
+                newValue == "0808" ||
+                newValue == "0701"
+            ) {
+                logo.setAttribute("src", "./Airtel.png")
+                return
+
+            }
+            if (newValue == "0805" ||
+                newValue == "0807" ||
+                newValue == "0811" ||
+                newValue == "0905"
+            ) {
+                logo.setAttribute("src", "Glo.webp")
+                return
+
+            }
+
+
+        }
+    })
+}
+// ======= DO NOT EDIT ============== //
+export default startApp;
+// ======= EEND DO NOT EDIT ========= //
